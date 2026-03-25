847469f02ecfccd4a68b552ba89d5f06a81e42d5
AkiraCoffee <88522379+Coffee8848@users.noreply.github.com>
2026-03-25 18:06:14 +1100

    bind string

--- app/src/main/java/com/example/travelcompanionapp/MainActivity.java
+++ app/src/main/java/com/example/travelcompanionapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.travelcompanionapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        spinnerCategory = findViewById(R.id.spinnerCategory);
+        spinnerFrom = findViewById(R.id.spinnerFrom);
+        spinnerTo = findViewById(R.id.spinnerTo);
+        editTextValue = findViewById(R.id.editTextValue);
+        buttonConvert = findViewById(R.id.buttonConvert);
+        textViewResult = findViewById(R.id.textViewResult);
+        String[] categories = {"Currency","Fuel","Temperature"};
+        ArrayAdapter<String> categoriesAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item,categories);
+        categoriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        String[] currency = {"USD", "AUD", "EUR", "JPY", "GBP"};
+        ArrayAdapter<String> currencyAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item,currency);
+        currencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerCategory.setAdapter(categoriesAdapter);
+        spinnerFrom.setAdapter(currencyAdapter);
+        spinnerTo.setAdapter(currencyAdapter);
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+    private Spinner spinnerCategory
+    private Spinner spinnerFrom;
+    private Spinner spinnerTo;
+    private EditText editTextValue;
+    private Button buttonConvert;
+    private TextView textViewResult;
+
 }
